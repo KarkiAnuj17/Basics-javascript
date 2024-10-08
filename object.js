4fc6a65dbad8c294286ed388b8bfd52de25743c2
Anuj Karki <karkianuj111@gmail.com>
2024-10-08 13:32:17 +0530

    basics

---
+++ object.js
@@ -0,0 +1,12 @@
+const product={
+  fullName:"pen",
+  price:270,
+  rating:4,
+  discount:5,
+};
+console.log(product);
+
+product.fullName = "bul pen";
+console.log(product.fullName);
+
+console.log(product)
---
+++ varLetConst.js
@@ -0,0 +1,11 @@
+console.log(x); // undefined (due to hoisting)
+var x = 5;
+console.log(x); 
+
+ // console.log(y); Cannot access 'y' before initialization
+ let y = 10;
+ console.log(y); 
+
+ const z = 15;
+ console.log(z); 
+
