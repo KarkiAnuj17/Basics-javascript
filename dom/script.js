c7dc84246bf04954ad6b4eac61e4c913c09d635a
Anuj Karki <karkianuj111@gmail.com>
2024-10-14 18:29:52 +0530

    dom

---
+++ dom/script.js
@@ -0,0 +1,17 @@
+//Selecting with id
+//->document.getElementById("myId");
+let header=document.getElementById("heading");
+console.log(header);
+
+//selecting with class->returs html collection
+//->documnet.getElementByClassName("MyClass")
+let headings=document.getElementsByClassName("header");
+console.log(headings);
+
+//selecting with tag
+//->document.getElementsByTagName("p")
+let para=document.getElementsByTagName("p");
+console.log(para);
+
+//query selector
+//->document.querySelector("myId/myClass/tag")
